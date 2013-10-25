bc7a5aa48d21b9052b8c8059df04fa83bac4a95d
Paul Oliver <puzza007@gmail.com>
2013-10-25 14:54:11 +0200

    Add folsom stats

--- src/riakc_poolboy.erl
+++ src/riakc_poolboy.erl
@@ -1,6 +1,5 @@
 -module(riakc_poolboy).
 
-%% @headerfile "riakc.hrl"
 -include_lib("riakc/include/riakc.hrl").
 
 -export([get_server_info/1, get_server_info/2,
@@ -21,187 +20,129 @@
 
 -type msg_id() :: non_neg_integer(). %% Request identifier for tunneled message types
 
--define(POOLBOY_TRANSACTION(X), poolboy:transaction(PoolName, fun(Worker) -> gen_server:call(Worker, X) end).
-
 -spec get_server_info(atom()) -> {ok, server_info()} | {error, term()}.
 get_server_info(PoolName) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, get_server_info)
-    end).
+    exec(PoolName, get_server_info).
 
 -spec get_server_info(atom(), timeout()) -> {ok, server_info()} | {error, term()}.
 get_server_info(PoolName, Timeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {get_server_info, Timeout})
-    end).
+    exec(PoolName, {get_server_info, Timeout}).
 
 -spec get(atom(), bucket(), key()) -> {ok, riakc_obj()} | {error, term()}.
 get(PoolName, Bucket, Key) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {get, Bucket, Key})
-    end).
+    exec(PoolName, {get, Bucket, Key}).
 
 -spec get(atom(), bucket(), key(), timeout() |  get_options()) ->
                  {ok, riakc_obj()} | {error, term()} | unchanged.
 get(PoolName, Bucket, Key, Timeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {get, Bucket, Key, Timeout})
-    end).
+    exec(PoolName, {get, Bucket, Key, Timeout}).
 
 -spec get(atom(), bucket(), key(), get_options(), timeout()) ->
                  {ok, riakc_obj()} | {error, term()} | unchanged.
 get(PoolName, Bucket, Key, Options, Timeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {get, Bucket, Key, Options, Timeout})
-    end).
+    exec(PoolName, {get, Bucket, Key, Options, Timeout}).
 
 -spec put(atom(), riakc_obj()) ->
                  ok | {ok, riakc_obj()} | {ok, key()} | {error, term()}.
 put(PoolName, Obj) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {put, Obj})
-    end).
+    exec(PoolName, {put, Obj}).
 
 -spec put(atom(), riakc_obj(), timeout() | put_options()) ->
                  ok | {ok, riakc_obj()} |  riakc_obj() | {ok, key()} | {error, term()}.
 put(PoolName, Obj, Timeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {put, Obj, Timeout})
-    end).
+    exec(PoolName, {put, Obj, Timeout}).
 
 -spec put(atom(), riakc_obj(), put_options(), timeout()) ->
                  ok | {ok, riakc_obj()} | riakc_obj() | {ok, key()} | {error, term()}.
 put(PoolName, Obj, Options, Timeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {put, Obj, Options, Timeout})
-    end).
+    exec(PoolName, {put, Obj, Options, Timeout}).
 
 -spec delete(atom(), bucket(), key()) -> ok | {error, term()}.
 delete(PoolName, Bucket, Key) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {delete, Bucket, Key})
-    end).
+    exec(PoolName, {delete, Bucket, Key}).
   
 -spec delete(atom(), bucket(), key(), timeout() | delete_options()) ->
                     ok | {error, term()}.
 delete(PoolName, Bucket, Key, Timeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {delete, Bucket, Key, Timeout})
-    end).
+    exec(PoolName, {delete, Bucket, Key, Timeout}).
 
 -spec delete(atom(), bucket(), key(), delete_options(), timeout()) -> ok | {error, term()}.
 delete(PoolName, Bucket, Key, Options, Timeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {delete, Bucket, Key, Options, Timeout})
-    end).
+    exec(PoolName, {delete, Bucket, Key, Options, Timeout}).
 
 -spec delete_vclock(atom(), bucket(), key(), riakc_obj:vclock()) -> ok | {error, term()}.
 delete_vclock(PoolName, Bucket, Key, VClock) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {delete_vclock, Bucket, Key, VClock})
-    end).
+    exec(PoolName, {delete_vclock, Bucket, Key, VClock}).
 
 -spec delete_vclock(atom(), bucket(), key(), riakc_obj:vclock(), timeout() | delete_options()) ->
                            ok | {error, term()}.
 delete_vclock(PoolName, Bucket, Key, VClock, TimeoutOrOptions) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {delete_vclock, Bucket, Key, VClock, TimeoutOrOptions})
-    end).
+    exec(PoolName, {delete_vclock, Bucket, Key, VClock, TimeoutOrOptions}).
 
 -spec delete_vclock(atom(), bucket(), key(), riakc_obj:vclock(), delete_options(), timeout()) ->
                            ok | {error, term()}.
 delete_vclock(PoolName, Bucket, Key, VClock, Options, Timeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {delete_vclock, Bucket, Key, VClock, Options, Timeout})
-    end).
+    exec(PoolName, {delete_vclock, Bucket, Key, VClock, Options, Timeout}).
 
 -spec delete_obj(atom(), riakc_obj()) -> ok | {error, term()}.
 delete_obj(PoolName, Obj) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {delete_obj, Obj})
-    end).
+    exec(PoolName, {delete_obj, Obj}).
 
 -spec delete_obj(atom(), riakc_obj(), delete_options()) -> ok | {error, term()}.
 delete_obj(PoolName, Obj, Options) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {delete_obj, Obj, Options})
-    end).
+    exec(PoolName, {delete_obj, Obj, Options}).
 
 -spec delete_obj(atom(), riakc_obj(), delete_options(), timeout()) -> ok | {error, term()}.
 delete_obj(PoolName, Obj, Options, Timeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {delete_obj, Obj, Options, Timeout})
-    end).
+    exec(PoolName, {delete_obj, Obj, Options, Timeout}).
 
 -spec list_buckets(atom()) -> {ok, [bucket()]} | {error, term()}.
 list_buckets(PoolName) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, list_buckets)
-    end).
+    exec(PoolName, list_buckets).
 
 -spec list_buckets(atom(), timeout()|list()) -> {ok, [bucket()]} |
                                                    {error, term()}.
 list_buckets(PoolName, Timeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {list_buckets, Timeout})
-    end).
+    exec(PoolName, {list_buckets, Timeout}).
 
 -spec list_buckets(atom(), timeout()|list(), timeout()|list()) -> {ok, [bucket()]} |
                                                    {error, term()}.
 list_buckets(PoolName, Timeout, CallTimeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {list_buckets, Timeout, CallTimeout})
-    end).
+    exec(PoolName, {list_buckets, Timeout, CallTimeout}).
 
 -spec list_keys(atom(), bucket()) -> {ok, [key()]} | {error, term()}.
 list_keys(PoolName, Bucket) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {list_keys, Bucket})
-    end).
+    exec(PoolName, {list_keys, Bucket}).
 
 -spec list_keys((atom), bucket(), list()|timeout()) -> {ok, [key()]} |
                                                       {error, term()}.
 list_keys(PoolName, Bucket, Timeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {list_keys, Bucket, Timeout})
-    end).
+    exec(PoolName, {list_keys, Bucket, Timeout}).
 
 -spec get_bucket(atom(), bucket()) -> {ok, bucket_props()} | {error, term()}.
 get_bucket(PoolName, Bucket) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {get_bucket, Bucket})
-    end).
+    exec(PoolName, {get_bucket, Bucket}).
 
 -spec get_bucket(atom(), bucket(), timeout()) -> {ok, bucket_props()} | {error, term()}.
 get_bucket(PoolName, Bucket, Timeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {get_bucket, Bucket, Timeout})
-    end).
+    exec(PoolName, {get_bucket, Bucket, Timeout}).
 
 -spec get_bucket(atom(), bucket(), timeout(), timeout()) -> {ok, bucket_props()} |
                                                            {error, term()}.
 get_bucket(PoolName, Bucket, Timeout, CallTimeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {get_bucket, Bucket, Timeout, CallTimeout})
-    end).
+    exec(PoolName, {get_bucket, Bucket, Timeout, CallTimeout}).
 
 -spec set_bucket(atom(), bucket(), bucket_props()) -> ok | {error, term()}.
 set_bucket(PoolName, Bucket, BucketProps) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {set_bucket, Bucket, BucketProps})
-    end).
+    exec(PoolName, {set_bucket, Bucket, BucketProps}).
 
 -spec set_bucket(atom(), bucket(), bucket_props(), timeout()) -> ok | {error, term()}.
 set_bucket(PoolName, Bucket, BucketProps, Timeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {set_bucket, Bucket, BucketProps,  Timeout})
-    end).
+    exec(PoolName, {set_bucket, Bucket, BucketProps,  Timeout}).
 
 -spec set_bucket(atom(), bucket(), bucket_props(), timeout(), timeout()) -> ok | {error, term()}.
 set_bucket(PoolName, Bucket, BucketProps, Timeout, CallTimeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {set_bucket, Bucket, BucketProps, Timeout, CallTimeout})
-    end).
+    exec(PoolName, {set_bucket, Bucket, BucketProps, Timeout, CallTimeout}).
 
 -spec mapred(atom(), mapred_inputs(), [mapred_queryterm()]) ->
                     {ok, mapred_result()} |
@@ -209,9 +150,7 @@ set_bucket(PoolName, Bucket, BucketProps, Timeout, CallTimeout) ->
                     {error, timeout} |
                     {error, term()}.
 mapred(PoolName, Inputs, Query) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {mapred, Inputs, Query})
-    end).
+    exec(PoolName, {mapred, Inputs, Query}).
 
 -spec mapred(atom(), mapred_inputs(), [mapred_queryterm()], timeout()) ->
                     {ok, mapred_result()} |
@@ -219,9 +158,7 @@ mapred(PoolName, Inputs, Query) ->
                     {error, timeout} |
                     {error, term()}.
 mapred(PoolName, Inputs, Query, Timeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {mapred, Inputs, Query, Timeout})
-    end).
+    exec(PoolName, {mapred, Inputs, Query, Timeout}).
 
 -spec mapred(atom(), mapred_inputs(), [mapred_queryterm()], timeout(), timeout()) ->
                     {ok, mapred_result()} |
@@ -229,9 +166,7 @@ mapred(PoolName, Inputs, Query, Timeout) ->
                     {error, timeout} |
                     {error, term()}.
 mapred(PoolName, Inputs, Query, Timeout, CallTimeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {mapred, Inputs, Query, Timeout, CallTimeout})
-    end).
+    exec(PoolName, {mapred, Inputs, Query, Timeout, CallTimeout}).
 
 -spec mapred_bucket(atom(), bucket(), [mapred_queryterm()]) ->
                            {ok, mapred_result()} |
@@ -239,9 +174,7 @@ mapred(PoolName, Inputs, Query, Timeout, CallTimeout) ->
                            {error, timeout} |
                            {error, term()}.
 mapred_bucket(PoolName, Bucket, Query) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {mapred_bucket, Bucket, Query})
-    end).
+    exec(PoolName, {mapred_bucket, Bucket, Query}).
 
 -spec mapred_bucket(atom(), bucket(), [mapred_queryterm()], timeout()) ->
                            {ok, mapred_result()} |
@@ -249,9 +182,7 @@ mapred_bucket(PoolName, Bucket, Query) ->
                            {error, timeout} |
                            {error, term()}.
 mapred_bucket(PoolName, Bucket, Query, Timeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {mapred_bucket, Bucket, Query, Timeout})
-    end).
+    exec(PoolName, {mapred_bucket, Bucket, Query, Timeout}).
 
 -spec mapred_bucket(atom(), bucket(), [mapred_queryterm()],
                     timeout(), timeout()) ->
@@ -260,69 +191,88 @@ mapred_bucket(PoolName, Bucket, Query, Timeout) ->
                            {error, timeout} |
                            {error, term()}.
 mapred_bucket(PoolName, Bucket, Query, Timeout, CallTimeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {mapred_bucket, Bucket, Query, Timeout, CallTimeout})
-    end).
+    exec(PoolName, {mapred_bucket, Bucket, Query, Timeout, CallTimeout}).
 
 -spec search(atom(), binary(), binary()) ->
                     {ok, search_result()} | {error, term()}.
 search(PoolName, Index, SearchQuery) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {search, Index, SearchQuery})
-    end).
+    exec(PoolName, {search, Index, SearchQuery}).
 
 -spec search(atom(), binary(), binary(), search_options()) ->
                     {ok, search_result()} | {error, term()}.
 search(PoolName, Index, SearchQuery, Options) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {search, Index, SearchQuery, Options})
-    end).
+    exec(PoolName, {search, Index, SearchQuery, Options}).
 
 -spec search(atom(), binary(), binary(), search_options(), timeout()) ->
                     {ok, search_result()} | {error, term()}.
 search(PoolName, Index, SearchQuery, Options, Timeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {search, Index, SearchQuery, Options, Timeout})
-    end).
+    exec(PoolName, {search, Index, SearchQuery, Options, Timeout}).
 
 -spec search(atom(), binary(), binary(), search_options(), timeout(), timeout()) ->
                     {ok, search_result()} | {error, term()}.
 search(PoolName, Index, SearchQuery, Options, Timeout, CallTimeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {search, Index, SearchQuery, Options, Timeout, CallTimeout})
-    end).
+    exec(PoolName, {search, Index, SearchQuery, Options, Timeout, CallTimeout}).
 
 -spec get_index(atom(), bucket(), binary() | secondary_index_id(), key() | integer()) ->
                        {ok, index_results()} | {error, term()}.
 get_index(PoolName, Bucket, Index, Key) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {get_index, Bucket, Index, Key})
-    end).
+    exec(PoolName, {get_index, Bucket, Index, Key}).
 
 -spec get_index(atom(), bucket(), binary() | secondary_index_id(), key() | integer(), timeout(), timeout()) ->
                        {ok, index_results()} | {error, term()}.
 get_index(PoolName, Bucket, Index, Key, Timeout, CallTimeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {get_index, Bucket, Index, Key, Timeout, CallTimeout})
-    end).
+    exec(PoolName, {get_index, Bucket, Index, Key, Timeout, CallTimeout}).
 
 -spec get_index(atom(), bucket(), binary() | secondary_index_id(), key() | integer(), key() | integer()) ->
                        {ok, index_results()} | {error, term()}.
 get_index(PoolName, Bucket, Index, StartKey, EndKey) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {get_index, Bucket, Index, StartKey, EndKey})
-    end).
+    exec(PoolName, {get_index, Bucket, Index, StartKey, EndKey}).
 
 -spec get_index(atom(), bucket(), binary() | secondary_index_id(), key() | integer() | list(),
                 key() | integer() | list(), timeout(), timeout()) ->
                        {ok, index_results()} | {error, term()}.
 get_index(PoolName, Bucket, Index, StartKey, EndKey, Timeout, CallTimeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {get_index, Bucket, Index, StartKey, EndKey, Timeout, CallTimeout})
-    end).
+    exec(PoolName, {get_index, Bucket, Index, StartKey, EndKey, Timeout, CallTimeout}).
 
 -spec tunnel(atom(), msg_id(), binary(), timeout()) -> {ok, binary()} | {error, term()}.
 tunnel(PoolName, MsgId, Pkt, Timeout) ->
-    poolboy:transaction(PoolName, fun(Worker) ->
-        gen_server:call(Worker, {tunnel, MsgId, Pkt, Timeout})
-    end).
+    exec(PoolName, {tunnel, MsgId, Pkt, Timeout}).
+
+-spec exec(atom(), atom() | tuple()) -> ok | tuple().
+exec(PoolName, X) ->
+    PoolNameBin = atom_to_binary(PoolName, latin1),
+    Name = list_to_binary([<<"riakc_poolboy.">>, PoolNameBin, <<".">> | stat(X)]),
+    Fun = fun(Worker) ->
+                  Metric = folsom_metrics:histogram_timed_begin(Name),
+                  Res = gen_server:call(Worker, X),
+                  ok = histogram_timed_notify(Metric),
+                  Res
+          end,
+    poolboy:transaction(PoolName, Fun).
+
+-spec stat(tuple() | atom()) -> [binary()].
+stat(T) when is_tuple(T) andalso
+             (element(1, T) =:= get orelse
+              element(1, T) =:= delete orelse
+              element(1, T) =:= delete_vclock orelse
+              element(1, T) =:= list_keys orelse
+              element(1, T) =:= get_bucket orelse
+              element(1, T) =:= set_bucket orelse
+              element(1, T) =:= mapred_bucket orelse
+              element(1, T) =:= get_index) ->
+    Op = element(1, T),
+    Bucket = element(2, T),
+    [atom_to_binary(Op, latin1), <<".">>, Bucket];
+stat(Op) when is_atom(Op) ->
+    [atom_to_binary(Op, latin1)];
+stat(T) when is_tuple(T) ->
+    Op = element(1, T),
+    [atom_to_binary(Op, latin1)].
+
+histogram_timed_notify({Name, _} = Metric) ->
+     try
+         ok = folsom_metrics:histogram_timed_notify(Metric)
+     catch _:_ ->
+             catch folsom_metrics:new_histogram(Name),
+             folsom_metrics:safely_histogram_timed_notify(Metric)
+     end.
