14da7aa28048bd68d2a70faa9ae8e6423d9cbb60
Aaron France <aaron.l.france@gmail.com>
2014-11-04 11:56:00 +0100

    Handle cases where checking pool status crashes

--- src/riakc_pool_status.erl
+++ src/riakc_pool_status.erl
@@ -12,7 +12,11 @@
                 ref :: reference(),
                 timer :: timer:tref()}).
 
--define(STAT_NAMES, ["workers", "overflow", "monitors"]).
+-define(STAT_NAMES, [{new_gauge, "workers"},
+                     {new_gauge, "overflow"},
+                     {new_gauge, "monitors"},
+                     {new_histogram, "stats_timeout"}
+                    ]).
 
 start_link(Name) when is_atom(Name) ->
     gen_server:start_link(?MODULE, [Name], []).
@@ -22,9 +26,9 @@ init([Name]) when is_atom(Name) ->
     NumWorkers = length(WorkerList),
     Ref = make_ref(),
     [begin
-         GaugeName = gauge_name(Name, StatName),
-         ok = folsom_metrics:new_gauge(GaugeName)
-     end || StatName <- ?STAT_NAMES],
+         MetricName = metric_name(Name, StatName),
+         ok = folsom_metrics:F(MetricName)
+     end || {F, StatName} <- ?STAT_NAMES],
     {ok, Tref} = timer:send_interval(1000, self(), {status, Ref}),
     process_flag(trap_exit, true),
     {ok, #state{name=Name, num_workers=NumWorkers, ref=Ref, timer=Tref}}.
@@ -36,18 +40,24 @@ handle_cast(_Msg, State) ->
     {stop, unhandled_cast, State}.
 
 handle_info({status, Ref}, State = #state{name=Name, ref=Ref}) ->
-    {_StatusName, WorkerQueueLen, Overflow, MonitorsSize} = poolboy:status(Name),
-    WorkerGauge   = gauge_name(Name, "workers"),
-    OverflowGauge = gauge_name(Name, "overflow"),
-    MonitorsGauge = gauge_name(Name, "monitors"),
-    ok = folsom_metrics:notify({WorkerGauge, WorkerQueueLen}),
-    ok = folsom_metrics:notify({OverflowGauge, Overflow}),
-    ok = folsom_metrics:notify({MonitorsGauge, MonitorsSize}),
-    {noreply, State}.
+    try
+        {_StatusName, WorkerQueueLen, Overflow, MonitorsSize} = poolboy:status(Name),
+        WorkerGauge   = metric_name(Name, "workers"),
+        OverflowGauge = metric_name(Name, "overflow"),
+        MonitorsGauge = metric_name(Name, "monitors"),
+        ok = folsom_metrics:notify({WorkerGauge, WorkerQueueLen}),
+        ok = folsom_metrics:notify({OverflowGauge, Overflow}),
+        ok = folsom_metrics:notify({MonitorsGauge, MonitorsSize})
+    catch
+        timeout:{gen_server, call, [_, status]} ->
+            Timeout = metric_name(Name, "stats_timeout"),
+            ok = folsom_metrics:notify({Timeout, 1})
+    end,
+    {noreply, State}.            
 
 terminate(_Reason, #state{name=Name, timer=Tref}) ->
     [begin
-         GaugeName = gauge_name(Name, StatName),
+         GaugeName = metric_name(Name, StatName),
          folsom_metrics:delete_metric(GaugeName)
      end || StatName <- ?STAT_NAMES],
     {ok, cancel} = timer:cancel(Tref),
@@ -56,6 +66,6 @@ terminate(_Reason, #state{name=Name, timer=Tref}) ->
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
-gauge_name(PoolName, StatName) ->
+metric_name(PoolName, StatName) ->
     NameString = atom_to_list(PoolName),
     list_to_binary(NameString ++ ".pool." ++ StatName).
